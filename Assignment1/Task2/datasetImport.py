33a11fa97e40dfbd87b40b11a1e0dbce8774e406
valerie <vbarker@unm.edu>
2026-02-11 14:26:07 -0700

    Added some normalization functions

--- Assignment1/Task2/datasetImport.py
+++ Assignment1/Task2/datasetImport.py
@@ -25,11 +25,11 @@ df_wine.columns = ['Class label', 'Alcohol',
 
 df_wine = df_wine[df_wine['Class label'].isin([1, 2])]
 
-X_wine = df_wine.iloc[:, 1:].values
+x_wine = df_wine.iloc[:, 1:].values
 y_wine = np.where(df_wine['Class label'].values == 2, 1, 0)
 
 print("Wine labels:", np.unique(y_wine, return_counts=True))
-print("Wine X shape:", X_wine.shape)
+print("Wine X shape:", x_wine.shape)
 
 
 # iris
@@ -45,8 +45,31 @@ df_iris = df_iris.dropna(subset=['class'])
 
 df_iris = df_iris[df_iris['class'].isin(['Iris-setosa', 'Iris-versicolor'])]
 
-X_iris = df_iris.iloc[:, 0:4].values
+x_iris = df_iris.iloc[:, 0:4].values
 y_iris = np.where(df_iris['class'].values == 'Iris-versicolor', 1, 0)
 
 print("Iris labels:", np.unique(y_iris, return_counts=True))
-print("Iris X shape:", X_iris.shape)
+print("Iris X shape:", x_iris.shape)
+
+def sigmoid (a) :
+    # Sigmoid function:
+    # Normalizes elements between 0 and 1
+    # Maintains order, so if x > y then
+    # sigmoid(x) > sigmoid(y)
+    return np.reciprocal(np.exp(-1 * a) + 1)
+
+def normalized(a) :
+    # Normalization function
+    # Normalizes elements between 0 and 1
+    # Maintains order, so if x > y then
+    # normalized(x) > normalized(y)
+    return (a - np.min(a))/(np.max(a) - np.min(a))
+
+def normalized(a,scale) :
+    # Scales the regular normalization function by specified value
+    return scale * (a - np.min(a))/(np.max(a) - np.min(a))
+
+
+print("wine x:",x_wine)
+print("sigmoid x:",sigmoid(x_wine))
+print("normalized x:",normalized(x_wine))
