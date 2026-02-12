f7ba87bbfa7eaf4f4967cb0a23d17ab7850f6d43
CatchSyntaxTerror <youssefaminbeh@gmail.com>
2026-02-12 15:53:12 -0700

    Task 3 completed

--- Assignment1/cerberus.py
+++ Assignment1/cerberus.py
@@ -14,8 +14,61 @@ def load_iris_all():
     df = df.dropna(subset=["class"])
 
     X = df.iloc[:, 0:4].values
-    y = df["class"].values  # strings
+    y = df["class"].values
     X = norm.normalized(X)
 
     return X, y
 
+class Perceptron:
+    """
+    Based off AdalineGD except it applies a hard cutoff and
+    only updates when its wrong 
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+        
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.1, size=X.shape[1] + 1)
+        
+        for i in range(self.n_iter):
+            for xi, target in zip(X,y): # lol haskell
+                xi_aug = np.append(xi, 1.0)
+                pred = 1 if np.dot(xi_aug, self.w_) >= 0.0 else 0
+                update = self.eta * (target-pred)
+                self.w_ += update * xi_aug
+        return self
+    
+    def net_input(self, X):
+        X_aug = np.hstack([X, np.ones((X.shape[0], 1))])
+        return np.dot(X_aug, self.w_)
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+    
+# train the perceptrons
+X, y = load_iris_all()
+
+p_setosa = Perceptron()
+p_versicolor = Perceptron()
+p_virginica = Perceptron()
+
+p_setosa.fit(X, (y == "Iris-setosa").astype(int))
+p_versicolor.fit(X, (y == "Iris-versicolor").astype(int))
+p_virginica.fit(X, (y == "Iris-virginica").astype(int))
+
+def predict_multiclass(X, models, class_names):
+    scores = np.vstack([m.net_input(X) for m in models])
+    winner = np.argmax(scores, axis=0)
+    return class_names[winner]
+
+models = [p_setosa, p_versicolor, p_virginica]
+class_names = np.array(["Iris-setosa", "Iris-versicolor", "Iris-virginica"])
+
+preds = predict_multiclass(X, models, class_names)
+
+classes, counts = np.unique(preds, return_counts=True)
+for i in range(len(classes)):
+    print(classes[i], ":", counts[i])
