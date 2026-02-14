1436ee627a52f1b87d30a3403100e3d761df1987
valerie <vbarker@unm.edu>
2026-02-13 19:04:12 -0700

    Added logistic regression stochastic version

--- Assignment1/compare_losses.py
+++ Assignment1/compare_losses.py
@@ -14,9 +14,9 @@ This plots the adaline and logistic regression
 
 # wine data sets 
 X_wine, y_wine = load_wine_data()
-ada_wine = ModifiedAdalineGD(eta=0.01, n_iter=50, random_state=1)
+ada_wine = ModifiedAdalineGD(eta=0.01, n_iter=10000, random_state=1)
 ada_wine.fit(X_wine, y_wine)
-log_wine = ModifiedLogisticRegressionGD(eta=0.01, n_iter=50, random_state=1)
+log_wine = ModifiedLogisticRegressionGD(eta=0.01, n_iter=10000, random_state=1)
 log_wine.fit(X_wine, y_wine)
 
 print("Adaline final loss:", ada_wine.losses_[-1])
@@ -25,9 +25,9 @@ print("Logistic final loss:", log_wine.losses_[-1])
 print("Logistic epochs:", len(log_wine.losses_))
 
 X_iris, y_iris = load_iris_data()
-ada_iris = ModifiedAdalineGD(eta=0.01, n_iter=50, random_state=1)
+ada_iris = ModifiedAdalineGD(eta=0.01, n_iter=10000, random_state=1)
 ada_iris.fit(X_iris, y_iris)
-log_iris = ModifiedLogisticRegressionGD(eta=0.01, n_iter=50, random_state=1)
+log_iris = ModifiedLogisticRegressionGD(eta=0.01, n_iter=10000, random_state=1)
 log_iris.fit(X_iris, y_iris)
 
 # wine
