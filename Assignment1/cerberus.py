35b4a0fe94bb93ec8087941195e3a143142b52b2
CatchSyntaxTerror <youssefaminbeh@gmail.com>
2026-02-15 16:05:35 -0700

    Updated n_iter in perceptron for a better repre4sentation of errors converging

--- Assignment1/cerberus.py
+++ Assignment1/cerberus.py
@@ -29,7 +29,7 @@ class Perceptron:
     removed bias by appending a 1 to each row.  
     """
 
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+    def __init__(self, eta=0.01, n_iter=100, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
