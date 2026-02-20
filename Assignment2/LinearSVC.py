603fae46708727d4e14723b82a59106b93d1ac8a
valerie <vbarker@unm.edu>
2026-02-20 16:18:04 -0700

    Addest tests for SVC model and debugged SVC model

--- Assignment2/LinearSVC.py
+++ Assignment2/LinearSVC.py
@@ -50,23 +50,32 @@ class LinearSVC:
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01,
         size=X.shape[1])
-        self.b_ = np.float_(0.)
-        self.errors_ = []
+        self.b_ = np.float64(0.)
+        self.class_errors_ = []
+        self.margin_violations_ = []
+        self.losses_ = []
         for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * self.loss_single(xi,target,C)
-                self.w_ += update * xi
-                self.b_ += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+            class_errors = 0
+            margin_violations = 0
+            for xi, yi in zip(X, y):
+                inside_margin = yi * (np.dot(xi, self.w_) + self.b_) >= 1
+                if inside_margin:
+                    self.w_ -= self.eta * self.w_
+                else:
+                    self.w_ -= self.eta * (self.w_ - C * yi * xi)
+                    self.b_ += self.eta * C * yi
+                if (self.predict(xi) != yi) : class_errors += 1
+                if (self.net_input(xi) * yi < 1) : margin_violations += 1
+            self.class_errors_.append(class_errors)
+            self.margin_violations_.append(margin_violations)
+            self.losses_.append(self.loss(X,y,C))
         return self
     
     # For SGD style, loss per datum
     def loss_single(self, Xi, yi, C) :
         hinge = max(0,1 - yi * np.dot(Xi,self.w_))
         squared_norm = 0.5 * np.dot(self.w_,self.w_)
-        return squared_norm + (C * hinge / Xi.shape[0])
+        return squared_norm + C * hinge
     
     # For mini or full batch, avg loss over data
     def loss(self, X, y, C):
@@ -86,4 +95,4 @@ class LinearSVC:
     # Unlie Adaline, we push to -1,1 which is standard for SVM
     def predict(self, X):
         """Return class label after unit step"""
-        return np.where(self.net_input(X) >= -1.0, 1,0)
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
--- Assignment2/test_LinearSVC.py
+++ Assignment2/test_LinearSVC.py
@@ -1,19 +1,72 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import make_classification as mc
 import LinearSVC as LSVC
+import MarginBand as mb
 
 #SVC parameters
-n = 10000
-l_rate = 0.01
-C = 1
+n = 100
+l_rate = 0.0001
+C = 200
 
 
 #Classification parameters
 u = 1
-n = 100
+n_samples = 1000
 d = 2
-X_train,y_train, X_test, y_test, a = mc.make_classification(d,n,u)
+X_train,y_train, X_test, y_test, a = mc.make_classification(d,n_samples,u)
 
 SVC_instance = LSVC.LinearSVC(eta=l_rate, n_iter=n, random_state=1)
 SVC_instance.fit(X_train,y_train,C)
+
+
+y_test_predict = SVC_instance.predict(X_test)
+
+# prediction class errors
+plt.figure()
+plt.plot(SVC_instance.class_errors_)
+plt.xlabel("Data")
+plt.ylabel("Class Errors")
+plt.title("SVC - Class Errors")
+plt.savefig("images/SVC_class_errors.png")
+plt.close()
+
+# prediction margin violations
+plt.figure()
+plt.plot(SVC_instance.margin_violations_)
+plt.xlabel("Data")
+plt.ylabel("Margin Violations")
+plt.title("SVC - Margin Violations")
+plt.savefig("images/SVC_margin_violations.png")
+plt.close()
+
+# prediction losses
+plt.figure()
+plt.plot(SVC_instance.losses_)
+plt.xlabel("Data")
+plt.ylabel("Loss")
+plt.title("SVC - Loss")
+plt.savefig("images/SVC_loss.png")
+plt.close()
+
+#plot of actual plane vs prediction
+#Separation Plane
+x_plot = np.linspace(-u,u,100)
+y_plot = -a[0]/a[1] * x_plot
+plt.plot(x_plot,y_plot)
+
+#Data and associated prediction
+colors = np.where(y_test_predict > 0, 'r','b')
+plt.scatter(
+    X_test[:,0], X_test[:,1],
+    c = colors)
+plt.ylim(-u,u)
+plt.title("prediction")
+plt.savefig("images/SVC_prediction.png")
+plt.close()
+
+#Margin Band
+mb.plot_svm_margin(X_test,y_test,SVC_instance)
+plt.ylim(-u,u)
+plt.savefig("images/SVC_band.png")
+plt.close()
