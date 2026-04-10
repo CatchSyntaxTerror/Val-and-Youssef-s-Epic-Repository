bfab10bc979f8c3938aee174a120482e2cf8b17a
Youssef <youssefaminbeh@gmail.com>
2026-04-09 18:16:40 -0600

    task 3 implemented

--- Assignment3/src/record.py
+++ Assignment3/src/record.py
@@ -1,24 +1,23 @@
 import os
-import csv
 
 """
 Functions for recording results
 """
-OUTDIR = os.path.join("data", "tables")
-os.makedirs(OUTDIR, exist_ok=True)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def save_baseline(config, results, use_test):
-
-    path = os.path.join("data", "tables")
+    path = os.path.join(BASE_DIR, "outputs", "tables")
     os.makedirs(path, exist_ok=True)
 
     file_path = os.path.join(path, "baseline_results.log")
     mode = "TEST" if use_test else "VALIDATION"
+    file_exists = os.path.exists(file_path)
 
     with open(file_path, "a") as f:
-        if not os.path.exists(file_path):
-            f.write("Baseline Results:\n\n")
+        if not file_exists:
+            f.write("Baseline Results:\n")
 
         f.write(f"{mode} RUN:\n")
         f.write(f"config: {config}\n")
         f.write(f"results: {results}\n")
+        f.flush()
--- Assignment3/src/timer.py
+++ Assignment3/src/timer.py
@@ -2,10 +2,9 @@ import time
 """
 A Val classic
 """
-
 class Timer():
     def __init__(self):
         self.time = time.time()
 
     def stop(self):
-        return self.time - time.time()
+        return time.time() - self.time
