5d5034ee696fd681cecd9bb8829db40a19e47b96
Youssef <youssefaminbeh@gmail.com>
2026-04-18 18:18:00 -0600

    wrote RNN

--- Assignment4/src/models.py
+++ Assignment4/src/models.py
@@ -1,3 +1,23 @@
+import torch
+import torch.nn as nn
 """
 Basline RNN, GRU and LSTM models
 """
+
+class RNN(nn.Module):
+    """
+    The BaseLine RNN modified from the textbook to take in the number of layers
+    """
+    def __init__(self,  input_size, hidden_size, num_layers):
+        super().__init__()
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        _, hidden = self.rnn(x)
+        out = hidden[-1,:,:]
+        out = self.fc(out)
+        return out
+    
+
+    
