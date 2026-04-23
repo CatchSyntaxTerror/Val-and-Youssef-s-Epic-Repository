efabd30d90d8e6eeb1039e25a8059afc5b457451
valerie <vbarker@unm.edu>
2026-04-23 08:12:07 -0600

    Added basic LSTM model

--- Assignment4/src/models.py
+++ Assignment4/src/models.py
@@ -17,5 +17,23 @@ class RNN(nn.Module):
         out = self.fc(out)
         return out
     
+class LSTM(nn.Module):
+    """Taken and adapted from the textbook"""
+    def __init__(self,rnn_hidden_size, fc_hidden_size,num_layers):
+        super().__init__
+        self.rnn = nn.LSTM(1,rnn_hidden_size,num_layers,batch_first=True)
+        self.fc1 = nn.Linear(rnn_hidden_size,fc_hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(fc_hidden_size,1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        _, (hidden, cell) = self.rnn(x)
+        out = hidden[-1,:,:]
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
 
     
